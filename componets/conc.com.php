3327af2784f3d9a1a874c32f65f563feb850ec46
Gibberlin <syedeyashin@gmail.com>
2026-03-08 23:22:30 +0530

    refractored the project for local use.

--- componets/conc.com.php
+++ componets/conc.com.php
@@ -1,11 +1,22 @@
 <?php
-$host = "sql302.infinityfree.com";
-$username = "if0_40974628";
-$password = "LacSxZWpY4am5";
-$database = "if0_40974628_student_db";
+//$host = "sql302.infinityfree.com";
+//$username = "if0_40974628";
+//$password = "LacSxZWpY4am5";
+//$database = "if0_40974628_student_db";
+
+//$conn = mysqli_connect($host, $username, $password, $database);
+
+//if (!$conn) {
+  //  die("Connection failed: " . mysqli_connect_error());
+//}
+
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "studentdb";
 
 $conn = mysqli_connect($host, $username, $password, $database);
-
+echo "Connetion established";
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
